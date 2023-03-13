82c66f110758b76af63aafc9ed29c8e365450b8f
Egon Willighagen <egon.willighagen@gmail.com>
2023-03-13 19:56:17 +0100

    Added a -v option to request info about the webservice on the command line (for use for Docker building, etc)

--- src/main/java/org/bridgedb/webservicetesting/BridgeDbWebservice/RestletServer.java
+++ src/main/java/org/bridgedb/webservicetesting/BridgeDbWebservice/RestletServer.java
@@ -1,7 +1,9 @@
 package org.bridgedb.webservicetesting.BridgeDbWebservice;
 
 import java.io.File;
+import java.util.Properties;
 
+import org.bridgedb.BridgeDb;
 import org.restlet.Component;
 import org.restlet.data.Protocol;
 
@@ -31,18 +33,28 @@ public class RestletServer {
 		}
 	}
 	
-	 public static void main(String[] args) throws Exception {
-			Class.forName ("org.bridgedb.rdb.IDMapperRdb");
-			int port = 8080; // default port
-			boolean cors = true;
-			if(args.length > 0) port = Integer. parseInt(args[0]);
-			if(args.length > 1) cors = !("false".equals(args[1]));
-			boolean transitive = false;
+    public static void main(String[] args) throws Exception {
+    	if (args.length > 0 && "-v".equals(args[0])) {
+			Properties props = new Properties();
+			props.load(BridgeDb.class.getClassLoader().getResourceAsStream("version.props"));
+			props.load(RestletServer.class.getClassLoader().getResourceAsStream("webservice.props"));
+    		System.out.println("java.version\t" + System.getProperty("java.version"));
+    		System.out.println("bridgedb.version\t" + props.getProperty("bridgedb.version"));
+    		System.out.println("webservice.version\t" + props.getProperty("webservice.version"));
+    		System.exit(0);
+    	} else {
+    		Class.forName ("org.bridgedb.rdb.IDMapperRdb");
+    		int port = 8080; // default port
+    		boolean cors = true;
+    		if(args.length > 0) port = Integer. parseInt(args[0]);
+    		if(args.length > 1) cors = !("false".equals(args[1]));
+    		boolean transitive = false;
 
-			File configFile = new File("./gdb.config");
-					    
-			RestletServer server = new RestletServer();
-			
-			server.run (port, configFile, transitive, cors);
-	 }
+    		File configFile = new File("./gdb.config");
+
+    		RestletServer server = new RestletServer();
+
+    		server.run (port, configFile, transitive, cors);
+    	}
+    }
 }
