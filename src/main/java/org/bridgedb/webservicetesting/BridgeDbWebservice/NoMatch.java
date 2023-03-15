d41ffab40cce50d812f3d53191046f68481943dd
Egon Willighagen <egon.willighagen@gmail.com>
2023-03-15 07:30:02 +0100

    Correct way to return the HTML

--- src/main/java/org/bridgedb/webservicetesting/BridgeDbWebservice/NoMatch.java
+++ src/main/java/org/bridgedb/webservicetesting/BridgeDbWebservice/NoMatch.java
@@ -1,12 +1,19 @@
 package org.bridgedb.webservicetesting.BridgeDbWebservice;
 
+import org.restlet.data.MediaType;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
 
-public class NoMatch extends RestletResource{
+public class NoMatch extends RestletResource {
+
 	@Get
-	public String getNoMatchResult() 
-	{
-		throw new IllegalArgumentException("Unrecognized query<p><font size='+1'><i>Double check the spelling and syntax. We are expecting something like: <a href='http://webservice.bridgedb.org/Human/xrefs/L/1234'>webservice.bridgedb.org/Human/xrefs/L/1234</a></i></font></p>");
+	public Representation getNoMatchResult() {
+		String error = "Unrecognized query<p><font size='+1'><i>Double check the spelling and syntax. We are expecting "
+				+ "something like: <a href='http://webservice.bridgedb.org/Human/xrefs/L/1234'>webservice.bridgedb.org/Human/xrefs/L/1234</a></i></font></p>";
+		StringRepresentation sr = new StringRepresentation(error);
+		sr.setMediaType(MediaType.TEXT_HTML);
+		return sr;
 	}
 
 }
